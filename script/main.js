c9e74399bd03beeb401d65967d64a995f11fe792
Gabriel Roec <104526610+gabrielroecc@users.noreply.github.com>
2023-10-06 10:52:54 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,15 +1,3 @@
-document.querySelector(".menu_btn").addEventListener("click", () => {
-	document.querySelector(".menu_window").classList.toggle("active");
-});
-document.querySelector(".menu_window").addEventListener("click", () => {
-	document.querySelector(".menu_window").classList.remove("active");
-});
-var menuLinks = document.querySelectorAll(".menu_link");
-menuLinks.forEach((el) => {
-	el.addEventListener("click", () => {
-		document.querySelector(".menu_window").classList.remove("active");
-	});
-});
 var div = document.querySelector(".contact");
 
 function irParaLink() {
