249a2119f650e539c2a7c2a388bcc8cdeb57976d
Gabriel Roec <104526610+gabrielroecc@users.noreply.github.com>
2023-10-06 11:26:58 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -5,3 +5,25 @@ function irParaLink() {
 }
 
 div.addEventListener("click", irParaLink);
+// Encontra a div com a classe cascata_img
+const cascataImgDiv = document.querySelector(".cascata_img");
+
+// Encontra o elemento com o ID cascata
+const cascataElement = document.getElementById("cascata");
+
+// Adiciona um evento de clique à div com a classe cascata_img
+cascataImgDiv.addEventListener("click", function () {
+	// Rola a página até o elemento com o ID cascata
+	cascataElement.scrollIntoView({ behavior: "smooth" }); // Isso suaviza a rolagem
+});
+// Encontra a div com a classe cascata_img
+const orgDiv = document.querySelector(".org_img");
+
+// Encontra o elemento com o ID cascata
+const orgElement = document.getElementById("organica");
+
+// Adiciona um evento de clique à div com a classe cascata_img
+orgDiv.addEventListener("click", function () {
+	// Rola a página até o elemento com o ID cascata
+	orgElement.scrollIntoView({ behavior: "smooth" }); // Isso suaviza a rolagem
+});
